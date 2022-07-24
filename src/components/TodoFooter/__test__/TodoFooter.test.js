46d0d50c61a3ccc916571eb35e0927d6e468cef8
JaneMoroz <moroz.jane@yahoo.com>
2022-07-24 13:03:48 +0300

    Test cleaning

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -11,22 +11,6 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
 };
 
 describe("TodoFooter", () => {
-  //   describe("Function 1", () => {
-  //     it("should render the correct amount of incomplete tasks", () => {
-  //       render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-  //       const paragraphElement = screen.getByText(/5 tasks left/i);
-  //       expect(paragraphElement).toBeInTheDocument();
-  //     });
-  //   });
-
-  //   describe("Function 2", () => {
-  //     it('should render "task" when the number of incomplete tasks is 1', () => {
-  //       render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  //       const paragraphElement = screen.getByText(/1 task left/i);
-  //       expect(paragraphElement).toBeInTheDocument();
-  //     });
-  //   });
-
   it("should render the correct amount of incomplete tasks", () => {
     render(<MockTodoFooter numberOfIncompleteTasks={5} />);
     const paragraphElement = screen.getByText(/5 tasks left/i);
